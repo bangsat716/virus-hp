dcd2fe417cb6ec3d83f37638b8ba0dacc6ba9a96
bangsat716 <61156264+bangsat716@users.noreply.github.com>
2020-02-19 13:55:31 +0700

    Add files via upload

---
+++ virus.py
@@ -0,0 +1,70 @@
+
+import os
+import sys
+import time
+import random
+os.system('clear')
+print ('\033[034mSUBSCRIBE dulu yt gue su_<')
+time.sleep(2)
+os.system("am start https://www.youtube.com/channel/UCVaKVhhcdngVG1aGjzDLFmg")
+os.system('clear')
+
+print ("""
+
+           $$\                               
+           \__|                              
+$$\    $$\ $$\  $$$$$$\  $$\   $$\  $$$$$$$\ 
+\$$\  $$  |$$ |$$  __$$\ $$ |  $$ |$$  _____|
+ \$$\$$  / $$ |$$ |  \__|$$ |  $$ |\$$$$$$\  
+  \$$$  /  $$ |$$ |      $$ |  $$ | \____$$\ 
+   \$  /   $$ |$$ |      \$$$$$$  |$$$$$$$  |
+    \_/    \__|\__|       \______/ \_______/ 
+                                             
+                                             
+                                             
+""")
+
+time.sleep(0.1)
+print ("""\033[031m[----------------TeaM-Cyber----------------]""")
+time.sleep(0.4)
+print ("\033[035m \033[034mVIRUS HP\033[037m  (\033[032m Versi 0.8\033[037m ) \033[037m")
+print ("""[--------:""")
+print ("\033[035m Author  : \033[032mDona \033[037m")
+print ("""[--------:""")
+print ("\033[035m\033[037m \033[031mYO\033[032mUT\033[034mUB\033[033mE :\033[032m TeaM Cyber \033[037m")
+print ("""[--------:""")
+print ("\033[035m \033[034mGithub\033[037m \033[033m : https://github.com/Maref0705/Dona\033[032m \033[037m \033[037m")
+print ("""\033[034m[------------------------------------------]""")
+time.sleep(0.5)
+def main(s):
+	  for c in s + '\n':
+		     sys.stdout.write(c)
+		     sys.stdout.flush()
+		     time.sleep(random.random() + 0.4)
+print ('\033[031mLoading.....')
+time.sleep(2)
+main('>>>>>>>>')
+time.sleep(1.2)
+skuy = input('nomor »--> :')
+if skuy == 'y':
+	print ('\033[033mTunggu Sebentar Su_<')
+	time.sleep(4)
+	
+elif skuy == "n":
+	print ('Selamat Datang')
+else:
+	print ("\033[034msalah Goblok!_<")
+	time.sleep(6)
+nomor = input('\033[371mMasuk Kan Nomor Korban: ' )
+time.sleep(2)
+main('\033[031mLoading.....')
+time.sleep(1.2)
+os.system('clear')
+print ("""\033[033m[--------TeaM-Cyber--------]""")
+print ('\033[032mMengirim virus ke nomor »-->' ,nomor)
+print ("""\033[033m[--------TeaM-Cyber--------]""")
+i = 1
+while i <= 100:
+	time.sleep(1.12)
+	print ('\033[031mMENGIRIM \033[032mVIRUS \033[034mKE NOMOR »-->\033[033m' ,nomor)
+	i += 1
